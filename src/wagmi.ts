0a06ea94f05c2c27910d79c9ef17f3e04ffced03
lohit <lohitsaidev@gmail.com>
2025-03-10 12:58:02 +0530

    Chore: Change wagmi.ts

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,13 +1,27 @@
 import { http, createConfig } from "wagmi";
 import { mainnet, sepolia } from "wagmi/chains";
-import { coinbaseWallet, injected, walletConnect } from "wagmi/connectors";
+import {
+  coinbaseWallet,
+  injected,
+  metaMask,
+  walletConnect,
+} from "wagmi/connectors";
 
 export const config = createConfig({
   chains: [mainnet, sepolia],
   connectors: [
+    metaMask({
+      checkInstallationImmediately: true,
+      openDeeplink(arg) {
+        console.log("openDeeplink", arg);
+      },
+    }),
     injected(),
     coinbaseWallet(),
-    walletConnect({ projectId: import.meta.env.VITE_WC_PROJECT_ID }),
+    walletConnect({
+      projectId: import.meta.env.VITE_WC_PROJECT_ID,
+      showQrModal: true,
+    }),
   ],
   transports: {
     [mainnet.id]: http(),
